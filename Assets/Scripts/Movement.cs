45c3792f9e5fc8e5e41a68377680bdfc9e887fc4
Blind56 <petrinko6@gmail.com>
2016-02-07 18:51:26 +0200

    Setup

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public Transform cam;
+    public float movementSpeed;
+    public float rotaionSpeed;
+
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movement
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed * Time.deltaTime);
+        cam.Rotate(Vector3.right * -Input.GetAxis("Mouse Y") * rotaionSpeed * Time.deltaTime);
+        transform.Rotate(transform.up * Input.GetAxis("Mouse X") * rotaionSpeed * Time.deltaTime);
+        Debug.DrawRay(transform.position, transform.right);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * Quaternion.AngleAxis(Input.GetAxis("Mouse Y"), Vector3.right), Time.deltaTime * 10);
+        
+    }
+}
---
+++ Assets/Scripts/StereoCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class StereoCamera : MonoBehaviour {
+
+    
+    public Transform leftCam;
+    public Transform rightCam;
+    public Transform aimPoint;
+    public float rayLenght = 100;
+    public float eyeDistance = 1;
+
+    private RaycastHit hit;
+
+    void Start ()
+    {
+        Vector3 eyeOffset = new Vector3(eyeDistance / 2, 0, 0);
+        leftCam.transform.localPosition = -eyeOffset;
+        rightCam.transform.localPosition = eyeOffset;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, rayLenght))
+        {
+            aimPoint.position = Vector3.Lerp(aimPoint.position, hit.point, Time.deltaTime * 10);            
+        }
+        else
+        {
+            aimPoint.localPosition = Vector3.Lerp(aimPoint.localPosition, Vector3.forward * rayLenght, Time.deltaTime * 10);
+        }
+
+        leftCam.LookAt(aimPoint);
+        rightCam.LookAt(aimPoint);
+
+        //Debug
+        Debug.DrawRay(ray.origin, ray.direction * rayLenght);
+        Debug.DrawRay(leftCam.transform.position, leftCam.transform.forward * rayLenght,Color.red);
+        Debug.DrawRay(rightCam.transform.position, rightCam.transform.forward * rayLenght, Color.green);
+    }
+}
